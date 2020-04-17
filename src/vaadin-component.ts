eea3337f725de84973d9942738bdc5186ab4d5c3
web-padawan <iamkulykov@gmail.com>
2020-04-17 11:13:58 +0300

    Add vaadin-element-mixin

--- src/vaadin-component.ts
+++ src/vaadin-component.ts
@@ -1,8 +1,9 @@
 import { PolymerElement, html } from '@polymer/polymer';
 import { ControlStateMixin } from '@vaadin/vaadin-control-state-mixin';
 import { ThemableMixin } from '@vaadin/vaadin-themable-mixin';
+import { ElementMixin } from '@vaadin/vaadin-element-mixin';
 
-export class TestComponent extends ControlStateMixin(ThemableMixin(PolymerElement)) {
+export class TestComponent extends ControlStateMixin(ThemableMixin(ElementMixin(PolymerElement))) {
   static get is() {
     return 'test-component';
   }
