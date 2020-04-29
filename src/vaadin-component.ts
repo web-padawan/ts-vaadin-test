ccf08a6fd8373110a70c511592d8fa79e115f593
web-padawan <iamkulykov@gmail.com>
2020-04-29 14:16:59 +0300

    Add vaadin-button element

--- src/vaadin-component.ts
+++ src/vaadin-component.ts
@@ -2,6 +2,7 @@ import { PolymerElement, html } from '@polymer/polymer';
 import { ControlStateMixin } from '@vaadin/vaadin-control-state-mixin';
 import { ThemableMixin } from '@vaadin/vaadin-themable-mixin';
 import { ElementMixin } from '@vaadin/vaadin-element-mixin';
+import '@vaadin/vaadin-button';
 
 export class TestComponent extends ControlStateMixin(ThemableMixin(ElementMixin(PolymerElement))) {
   static get is() {
@@ -11,11 +12,20 @@ export class TestComponent extends ControlStateMixin(ThemableMixin(ElementMixin(
   static get template() {
     return html`
       <button id="button">Button</button>
+
+      <vaadin-button>Button</vaadin-button>
     `
   }
 
   get focusElement() {
     return this.$.button;
+  }
+
+  ready() {
+    super.ready();
+
+    const button = this.shadowRoot?.querySelector('vaadin-button')!;
+    button.disabled = true;
   }
 }
 
